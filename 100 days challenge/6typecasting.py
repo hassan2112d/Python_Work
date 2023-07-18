71b7cf803c0a36b93ae0ffeba48a9856bd1de0cc
hassan2112d <hassan2112d@aptechgdn.net>
2023-07-19 01:09:01 +0500

    python1

---
+++ 100 days challenge/6typecasting.py
@@ -0,0 +1,15 @@
+# //Data Types //
+# Explicist Data Types
+# a = 2
+a = "2"
+
+# b = 2
+b = "2"
+
+print(a+b)
+
+print(int(a)+int(b))
+
+# Implicist Data Types
+
+print(type(a))
---
+++ 100 days challenge/7input.py
@@ -0,0 +1,10 @@
+a = input("Enter Your Name: \n")
+
+print("Your name is ",a)
+
+
+a = int(input("Enter First Number : \n" ))
+b = int(input("Enter Second Number : \n" ))
+c = int(input("Enter Third Number : \n" ))
+
+print(a+b+c)
