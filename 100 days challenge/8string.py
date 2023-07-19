61740f5082fc864188152ce40f5e714b1ca0206f
hassan2112d <hassan2112d@aptechgdn.net>
2023-07-20 00:29:23 +0500

    ok

---
+++ 100 days challenge/8string.py
@@ -0,0 +1,27 @@
+
+# names = "Harry"
+
+# print(len(names))
+
+# print(names[0:5])
+
+# ##
+
+# fruit = "Mangoes"
+
+# len1 = len(fruit)
+
+# print("Mangoes has",len1,"Letters")
+
+###
+
+fruits = "Mango"
+
+length = len(fruits)
+
+print(length)
+print(fruits[0:7])
+print(fruits[0:])
+print(fruits[:6])
+print(fruits[0:-3])
+print(fruits[-4:-2])
---
+++ 100 days challenge/9stringmethods.py
@@ -0,0 +1,9 @@
+a = "Harry!!!!!!!!!!!! hasn ali"
+
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Harry","hassan"))
+print(a.capitalize())
+print(a.split())
