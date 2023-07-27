640314484367f911cd8b679b296216f63da5c64f
hassan2112d <hassan2112d@aptechgdn.net>
2023-07-27 15:11:52 +0500

    python2

---
+++ 100 days challenge/forloop.py
@@ -0,0 +1,29 @@
+# string
+
+# a = "hello my name is hassan"
+
+# for i in a:
+#     print(i)
+#     if(i== "o"):
+#         print("This is O")
+
+
+# List
+# cd = ["Red","Green","Blue","purple","Yellow"]
+
+# for color in cd:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# Range builtin function
+
+# print 1 to 10
+
+# for i in range(1,10):
+#     print(i)
+
+# skipping 2 numbers
+
+for i in range(1,10,2):
+    print(i)
