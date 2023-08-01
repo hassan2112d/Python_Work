8c9230269423275a48a5d9cb80c5e9195119bd8c
hassan2112d <hassan2112d@aptechgdn.net>
2023-08-01 12:42:01 +0500

    python

---
+++ 100 days challenge/whileloop.py
@@ -0,0 +1,19 @@
+ 
+a = 0
+
+while (a<10):
+    print(a)
+    a = a +1
+
+print("10 numbers print successfully")
+
+# //decrementing While loop
+
+count = 5
+
+while (count>0):
+    print(count)
+
+    count = count -1
+else:
+    print("Loop ended successfully")
