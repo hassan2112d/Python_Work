595fcb92a7e8fc414568f65de68c30725c776b59
hassan2112d <hassan2112d@aptechgdn.net>
2023-08-11 20:18:47 +0500

    hloo

---
+++ 100 days challenge/listmethods.py
@@ -0,0 +1,32 @@
+
+l =[1,2,3,4,5,9,2]
+
+print(l)
+
+# Methods append add no
+
+l.append(7)
+
+print(l)
+
+#Method for sort
+#reverse descending order
+l.sort(reverse=True)
+
+print(l)
+
+print(l.count(2))
+
+l.insert(7,99)
+
+print(l)
+
+m = [999,1000,11000]
+
+l.extend(m)
+
+print(l)
+
+k = l + m 
+
+print(l)
