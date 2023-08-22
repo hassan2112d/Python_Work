133ae4dcce864f48089169b642b89b5120e77f23
hassan2112d <hassan2112d@aptechgdn.net>
2023-08-23 04:00:47 +0500

    hello

---
+++ 100 days challenge/tuple.py
@@ -0,0 +1,13 @@
+# Tuple cannot be changed = ()
+# list canbe changed =[]
+
+
+a = (1,2,4,5,6,"Hassan",True)
+
+print(type(a),a)
+print(len(a))
+
+if 3 in a:
+    print("6 is in a varaiable")
+else:
+    print('not found')
