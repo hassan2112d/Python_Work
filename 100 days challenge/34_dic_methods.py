c737c372c527a2d6d20562a4a74877044b225991
hassan2112d <hassan2112d@aptechgdn.net>
2023-09-30 04:55:16 +0500

    hiii

---
+++ 100 days challenge/34_dic_methods.py
@@ -0,0 +1,23 @@
+a = {
+    1 : 78,
+    2 : 98,
+    3 : 68,
+    4 : 48
+}
+
+b = {
+    5 : 72,
+    6 : 84,
+    7 : 65,
+    8 : 77
+}
+
+a.update(b)
+
+print(a)
+
+# Update update the data
+
+# pop() function will delete 1 data
+
+# .clear() will clear the dictonaries
---
+++ 100 days challenge/35_forloop_else_st_.py
@@ -0,0 +1,8 @@
+
+# when loop is finished then it will take me to else
+for i in range(5):
+    print(i)
+    
+else:
+    print("Completed")
+
